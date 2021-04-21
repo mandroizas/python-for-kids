bbe0c31f92c59e0be4f4e48a3ef625c171553ace
mandroizas <mandroizass@gmail.com>
2021-04-21 22:25:35 +0500

    WhatsMyGrade

---
+++ WhatsMyGrade.py
@@ -0,0 +1,11 @@
+grade = eval(input("Введите количество баллов (0-100): "))
+if grade >= 90:
+    print("У вас 5!:) ")
+elif grade >= 80:
+    print("У вас 4!")
+elif grade >= 70:
+    print("У вас 3.")
+elif grade >= 60:
+    print("У вас 2...")
+else:
+    print("У вас 1. :(")
