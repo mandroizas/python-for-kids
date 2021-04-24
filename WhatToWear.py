1a18b8bdaa13291b33a8e53fdea1e2bf6efa8059
mandroizas <mandroizass@gmail.com>
2021-04-24 17:41:51 +0500

    WhatToWhear

---
+++ WhatToWear.py
@@ -0,0 +1,10 @@
+rainy = input("Как погодка? Идёт дождь? (д/н) ").lower()
+cold = input("На улице холодно? (д/н) ").lower()
+if (rainy == 'д' and cold == 'д'):
+    print("Лучше наденьте плащ.")
+elif (rainy == 'д' and cold!= 'д'):
+    print("Возьмите с собой зонт.")
+elif (rainy!= 'д' and cold == 'д'):
+    print("Наденьте пальто: на улице холодно!")
+elif (rainy!= 'д' and cold!= 'д'):
+    print("Надевайте, что хотите: на улице прекрасно!")
