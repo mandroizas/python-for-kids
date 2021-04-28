2c42081b18e568206cb7b35c59c0140f437a0016
mandroizas <mandroizass@gmail.com>
2021-04-28 20:44:11 +0500

    EncoderDecoder

---
+++ EncoderDecoder.py
@@ -0,0 +1,12 @@
+message = input("Введите сообщение для кодирования или раскодирования: ")
+message = message.upper()
+output = ""
+for letter in message:
+    if letter.isupper():
+        value = ord(letter) + 13
+        letter = chr(value)
+    if not letter.isupper():
+        value -= 26
+        letter = chr(value)
+        output += letter
+print("Выходное сообщение: ", output)
--- WhatToWear.py
+++ WhatToWear.py
@@ -2,9 +2,9 @@ rainy = input("Как погодка? Идёт дождь? (д/н) ").lower()
 cold = input("На улице холодно? (д/н) ").lower()
 if (rainy == 'д' and cold == 'д'):
     print("Лучше наденьте плащ.")
-elif (rainy == 'д' and cold!= 'д'):
+elif (rainy == 'д' and cold != 'д'):
     print("Возьмите с собой зонт.")
-elif (rainy!= 'д' and cold == 'д'):
+elif (rainy != 'д' and cold == 'д'):
     print("Наденьте пальто: на улице холодно!")
-elif (rainy!= 'д' and cold!= 'д'):
+elif (rainy != 'д' and cold != 'д'):
     print("Надевайте, что хотите: на улице прекрасно!")
